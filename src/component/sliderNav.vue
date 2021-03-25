b6b4f03ee7830257cd264e235f47045199a199e9
ning <1427422460@qq.com>
2021-03-25 20:04:52 +0800

    系统初始布局

---
+++ src/component/sliderNav.vue
@@ -0,0 +1,34 @@
+<!--  -->
+<template>
+    <div class="main-header">
+        <a-button type="primary" style="margin-bottom: 16px" @click="toggleCollapsed">
+            <a-icon :type="collapsed ? 'menu-unfold' : 'menu-fold'" />
+        </a-button>
+        <div class="breadcrumb">
+            <Breadcrumb>
+                <Breadcrumb.Item>首页</Breadcrumb.Item>
+                <Breadcrumb.Item>
+                    <a href="">统计</a>
+                </Breadcrumb.Item>
+            </Breadcrumb>
+        </div>
+        <ul class="user-info">
+            <li>欢迎XXXXX
+                <a-icon type="down" />
+            </li>
+            <li>退出</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {}
+        }
+    }
+</script>
+
+<style lang='scss'>
+
+</style>
---
+++ src/layout/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <LeftMenu />
+    <!-- 只有左边菜单缩进去了就让他触发 menu-unfold-->
+    <div :class="{'main-app':true,'menu-unfold' : collapsed}">
+      <SliderNav />
+    </div>
+  </div>
+</template>
+
+<script>
+  import LeftMenu from '@/component/menu.vue';
+  import SliderNav from '@/component/sliderNav.vue';
+  export default {
+    data() {
+      return {
+        collapsed: false,
+      };
+    },
+    components: {
+      LeftMenu,
+      SliderNav
+    },
+    methods: {
+      toggleCollapsed() {
+        this.collapsed = !this.collapsed;
+      }
+    }
+  }
+</script>
+<style lang="less">
+  // 加上一个~让他当成模块来进行编译
+  @import url("~@/assets/css/home.less");
+</style>
